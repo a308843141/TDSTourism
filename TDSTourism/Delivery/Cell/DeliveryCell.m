a4fce58f9371691848f3df4e02116ec482de4fcf
qpple <qpple@qppledemacbook-pro.local>
2018-10-19 09:19:45 +0800

    cell

---
+++ TDSTourism/Delivery/Cell/DeliveryCell.m
@@ -0,0 +1,24 @@
+//
+//  DeliveryCell.m
+//  TDSTourism
+//
+//  Created by qpple on 2018/10/19.
+//  Copyright © 2018 qpple. All rights reserved.
+//
+
+#import "DeliveryCell.h"
+
+@implementation DeliveryCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ TDSTourism/Wish/Model/WishModel.m
@@ -0,0 +1,13 @@
+//
+//  WishModel.m
+//  TDSTourism
+//
+//  Created by qpple on 2018/10/19.
+//  Copyright © 2018 qpple. All rights reserved.
+//
+
+#import "WishModel.h"
+
+@implementation WishModel
+
+@end
